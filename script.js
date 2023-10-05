12e76fa2f26ab1df101fb30e8d1ae52c65a38b39
Melisha Shakya <melishas@buffalo.edu>
2023-10-04 19:27:42 -0700

    Changed the frontend design along with resume

--- script.js
+++ script.js
@@ -1,93 +1,62 @@
-window.onscroll = function() {scrolling()};
+const mediaQuery = window.matchMedia('(max-width: 550px)');
+const mediaQuery2 = window.matchMedia('(min-width: 550px)');
+let collapse=true;
+// Check if the media query is true
 
-function scrolling() {
-    
-
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.getElementById("topnav").style.backgroundColor = "rgba(255, 255, 255, 0.548)";
-        document.getElementById("topnav").style.color = "rgb(0,0,0, .7)";
-        tags=document.getElementsByTagName("a")
-        for (var i = 0; i<tags.length; i++) {
-            tags[i].style.color= "rgb(0,0,0, .7)";
-        }
-        document.getElementById("mobile_topnav").style.backgroundColor = "rgba(255, 255, 255, 0.548)";
-        document.getElementById("mobile_topnav").style.color = "rgb(0,0,0, .7)";
-        document.getElementById("mobile_nav").style.color="rgb(255,255,255)";
-        document.getElementById("mx").style.color="rgb(255,255,255)";
-        document.getElementById("mhome_active").style.color="rgb(255,255,255)";
-        document.getElementById("mabout_active").style.color="rgb(255,255,255)";
-        document.getElementById("mproject_active").style.color="rgb(255,255,255)";
-        document.getElementById("mcontact_active").style.color="rgb(255,255,255)";
-        
-        document.getElementById("home_active").style.color = "rgb(255, 255, 255)";
-        /*document.getElementById("topnav").style.top = "0";
-        document.getElementById("topnav").style.position = "sticky";*/
-    }
-    else {
-        document.getElementById("topnav").style.backgroundColor = "rgb(255, 255, 255, .2)";
-        document.getElementById("topnav").style.color = "rgb(255,255,255)";
-        document.getElementById("topnav").style.color = "rgb(255,255,255)";
-        tags=document.getElementsByTagName("a")
-        for (var i = 0; i<tags.length; i++) {
-            tags[i].style.color= "rgb(255,255,255)";
-        }
-        tags=document.getElementsByClassName("active")
-        for (var i = 0; i<tags.length; i++) {
-            tags[i].style.color= "rgb(94 38 38)";
-        }
-        document.getElementById("home_active").style.color = "rgb(16,202,219)";
-
-        document.getElementById("mobile_topnav").style.backgroundColor = "rgb(255, 255, 255, .2)";
-        document.getElementById("mobile_topnav").style.color = "rgb(255,255,255)";
-        document.getElementById("mobile_topnav").style.color = "rgb(255,255,255)";
-        
-        /*document.getElementById("topnav").style.top = "-50px";
-        document.getElementById("topnav").style.position = "unset";*/
-    }
-
-    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
-        document.getElementById("about").style.opacity="1";
-        document.getElementById("about").style.transition="1s all ease-in-out"
-    }
-    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 500) {
-        document.getElementById("project").style.opacity="1";
-        document.getElementById("project").style.transition="1s all ease-in-out"
-    }
-    /*if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
-        document.getElementById("contact").style.opacity="1";
-        document.getElementById("contact").style.transition="1s all ease-in-out"
-    }*/
-    if ((document.body.offsetHeight - document.documentElement.scrollTop)<= (window.innerHeight+6)) {
-        document.getElementById("contact").style.opacity="1";
-        document.getElementById("contact").style.transition="1s all ease-in-out"
-    }
-    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-        document.getElementById("home_active").style.color = "rgb(0,0,0)";
-        document.getElementById("about_active").style.color = "rgb(255, 255, 255)";
-        document.getElementById("project_active").style.color = "rgb(0,0,0)";
-        document.getElementById("contact_active").style.color = "rgb(0,0,0)";
-    }
-    if (document.body.scrollTop > 900 || document.documentElement.scrollTop > 900) {
-        document.getElementById("home_active").style.color = "rgb(0,0,0)";
-        document.getElementById("about_active").style.color = "rgb(0,0,0)";
-        document.getElementById("project_active").style.color = "rgb(255, 255, 255)";
-        document.getElementById("contact_active").style.color = "rgb(0,0,0)";
-        
-
-        
-    }
-    if((document.body.offsetHeight - document.documentElement.scrollTop)<= (window.innerHeight+4)){
-        document.getElementById("home_active").style.color = "rgb(0,0,0)";
-        document.getElementById("about_active").style.color = "rgb(0,0,0)";
-        document.getElementById("project_active").style.color = "rgb(0,0,0)";
-        document.getElementById("contact_active").style.color = "rgb(255, 255, 255)";
-    }
+function handleTabletChange(e){
+  if (e.matches) {
+  // Then trigger an alert
+  console.log("hehehe")
+  document.getElementById("prj-img1").src="https://media.istockphoto.com/id/1367980878/vector/futuristic-geometric-deep-blue-gradation-background-illustration.jpg?s=612x612&w=0&k=20&c=3s7xQfMSUcBQq_ZNTruiz-qELXenAPGnGypj4Z-daAE=";;
+  document.getElementById("prj-img2").src="https://media.istockphoto.com/id/1367980878/vector/futuristic-geometric-deep-blue-gradation-background-illustration.jpg?s=612x612&w=0&k=20&c=3s7xQfMSUcBQq_ZNTruiz-qELXenAPGnGypj4Z-daAE=";;
+  document.getElementById("prj-img3").src="https://media.istockphoto.com/id/1367980878/vector/futuristic-geometric-deep-blue-gradation-background-illustration.jpg?s=612x612&w=0&k=20&c=3s7xQfMSUcBQq_ZNTruiz-qELXenAPGnGypj4Z-daAE=";;
+  document.getElementById("prj-img4").src="https://media.istockphoto.com/id/1367980878/vector/futuristic-geometric-deep-blue-gradation-background-illustration.jpg?s=612x612&w=0&k=20&c=3s7xQfMSUcBQq_ZNTruiz-qELXenAPGnGypj4Z-daAE=";;
+}else{
+  document.getElementById("prj-img1").src="studyseeker.png";
+  document.getElementById("prj-img2").src="producepong.png";
+  document.getElementById("prj-img3").src="conways.png";
+  document.getElementById("prj-img4").src="carpoolbull.png";
+}
 }
 
-function openNav() {
-    document.getElementById("mobile_nav").style.width = "100%";
+function handleMenuBar(){
+  if (collapse){
+    collapse=false;
+    document.getElementById("top_nav").style.height="150px";
+    document.getElementById("mobile_nav").style.opacity="1";
+    document.getElementById("mobile_nav").style.right="0px";
+    document.getElementById("mobile_link").style.opacity="1";
+    document.getElementById("mobile_link").style.right="0px";
+  }
+  else{
+    collapse=true;
+    document.getElementById("top_nav").style.height="50px";
+    document.getElementById("mobile_nav").style.opacity="0";
+    document.getElementById("mobile_nav").style.right="-100%";
+    document.getElementById("mobile_link").style.opacity="0";
+    document.getElementById("mobile_link").style.right="-100%";
   }
   
-  function closeNav() {
-    document.getElementById("mobile_nav").style.width = "0";
+}
+
+function handleNavChange(e){
+  if (e.matches) {
+    collapse=true;
+    document.getElementById("top_nav").style.height="50px";
+    document.getElementById("mobile_nav").style.opacity="0";
+    document.getElementById("mobile_nav").style.right="-100%";
+    document.getElementById("mobile_link").style.opacity="0";
+    document.getElementById("mobile_link").style.right="-100%";
   }
+}
+
+mediaQuery.addListener(handleTabletChange);
+handleTabletChange(mediaQuery);
+
+mediaQuery2.addListener(handleNavChange);
+handleNavChange(mediaQuery2);
+
+const menubar= document.getElementById("fa-bars");
+menubar.addEventListener("click",handleMenuBar);
+
+document.getElementById("mobile_link").addEventListener("click",handleMenuBar);
